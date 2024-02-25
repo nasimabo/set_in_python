f38f4168759f231f240807325d35f9c907c447b1
nasimabo <nasimuddin555k@gmail.com>
2024-02-25 13:43:46 +0600

    set_in_python

---
+++ set.py
@@ -0,0 +1,88 @@
+##################### Set Python ########################
+
+'''set_var = {1,2,2,3,3,4,"nasim","rasel", "nasim"}
+
+print(set_var)
+print(type(set_var))'''
+
+'''var = set() #empty set # syntex
+print(var)
+print(type(var))'''
+
+
+################### Set method #################
+
+'''set_var = {1,2,3,4,5,6}
+
+set_var.add(7)
+set_var.add(8)
+set_var.add(9)
+set_var.add(10)
+
+print(set_var)
+
+set_var.remove(9)
+set_var.remove(5)
+set_var.remove(3)
+
+print(set_var)
+
+
+set_var.pop()
+set_var.pop()
+set_var.pop()
+
+print(set_var)
+
+set_var.clear()
+
+print(set_var)'''
+
+'''set1 = {1,2,3}
+set2 = {2,3,4}
+
+print(set1.union(set2))
+
+print(set1.intersection(set2))'''
+
+
+################ pactice deictionary and set ################
+
+'''dictionary = {}
+
+sub1 = int(input("enter che number: "))
+sub2 = int(input("enter phy number: "))
+sub3 = int(input("enter math number: "))
+
+dictionary.update({"che":sub1})
+dictionary.update({"phy":sub2})
+dictionary.update({"math":sub3})
+
+print(dictionary)'''
+
+
+'''values = {9,"9.0"}
+print(values)'''
+
+
+values = {
+    ("float",9.0),
+    ("int",9)
+    }
+print(values)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
